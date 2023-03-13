48b5a5cb6c1877b5658011796272f769568a4286
Ryan Hayes <rrh5210@psu.edu>
2023-03-12 20:17:50 -0400

    I already knew all this stuff

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 let age = 32;
 let userName = "Max";
 let hobbies = ["Sports", "Cooking", "Reading"];
-let job = { title: "Developer", place: "New York", salary: 50000 };
+let job = {
+  title: "Developer",
+  place: "New York",
+  salary: 50000,
+};
