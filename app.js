afe6f2e460b48930defda14d727dd224b69c492b
Ryan Hayes <rrh5210@psu.edu>
2023-03-30 19:41:10 -0400

    Day 34 complete

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 // console.dir(document);
 
-document.body.children[1].children[0].href = "https://www.google.com";
+// document.body.children[1].children[0].href = "https://www.google.com";
+
+// let anchorElement = document.getElementById("external-link");
+// anchorElement.href = "https://www.google.com";
+
+// anchorElement = document.querySelector("#external-id");
+// anchorElement.href = "https://academind.com";
+
+//ADD AN ELEMENT
+// 1. Create new element
+let newAnchor = document.createElement("a");
+newAnchor.href = "https://google.com";
+newAnchor.textContent = "\n This is a link to Google!";
+// 2. Get access to the parent element that should hold the new element
+let firstParagraph = document.querySelector("p");
+// 3. Insert the new element into the parent element content
+
+firstParagraph.append(newAnchor);
---
+++ exercise.js
@@ -0,0 +1,14 @@
+let heading = document.body.children[0];
+console.dir(heading);
+
+let body = heading.parentElement;
+console.dir(body);
+
+let paragraph = heading.nextElementSibling;
+console.dir(paragraph);
+
+let h1Element = document.getElementById("heading");
+console.dir(h1Element);
+
+let secondParagraph = document.querySelector(".second-p");
+console.dir(secondParagraph);
